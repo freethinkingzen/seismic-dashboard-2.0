5db0cf796775f69657b86a10e14cf4e139668399
John Lewis <johnlewis208503@gmail.com>
2023-12-24 14:09:15 -0700

    api info in data cards

--- src/components/DataCards.js
+++ src/components/DataCards.js
@@ -1,14 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Button, Card, CardContent, Divider, Grid, Skeleton, Typography } from '@mui/material';
+import {
+    Button,
+    Card,
+    CardContent, 
+    Divider, 
+    Grid, 
+    Skeleton, 
+    Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 import { SeismicDataContext } from '../Context';
-import { largestMagnitude } from '../utils/DataParser';
+import { largestMagnitude, significantQuakes, tsunamiPotential } from '../utils/DataParser';
 
 const DataCard = styled(Card)(({ theme }) => ({
     display: "flex",
     textAlign: 'center',
     justifyContent: "center",
-    minHeight: "8em",
+    height: "100%",
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
 }));
@@ -16,34 +24,39 @@ const DataCard = styled(Card)(({ theme }) => ({
 const DataCards = () => {
     const context = useContext(SeismicDataContext);
     const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
     const [totalQuakes, setTotalQuakes] = useState(0);
     const [largestMag, setLargestMag] = useState({});
+    const [significant, setSignificant] = useState([]);
+    const [tsunami, setTsunami] = useState([]);
+    const [selected, setSelected] = useState('');
 
     useEffect(() => {
         if (context.seismicDataToday.length > 0) {
-            setData(context.seismicDataToday);
-            setTotalQuakes(context.seismicDataToday.length - 1);
+            setTotalQuakes(context.seismicDataToday.length);
             setLargestMag(largestMagnitude(context.seismicDataToday));
+            setSignificant(significantQuakes(context.seismicDataToday));
+            setTsunami(tsunamiPotential(context.seismicDataToday));
             setLoading(false);
         }
     }, [context.seismicDataToday]);
 
     return (
         <Grid container spacing={1} sx={{ flexGrow: 1, padding: "8px" }}>
+
             <Grid item xs={12}>
-                <Typography variant="h5" color={"primary.contrastText"} sx={{ margin: "1px", textAlign: "center" }}>
+                <Typography variant="h5" color={"primary.contrastText"} sx={{ textAlign: "center" }}>
                     Today at a Glance
                 </Typography>
             </Grid>
+
             <Grid item xs={6} sm={3} md={12}>
                 <DataCard>
                     <CardContent>
-                        <Typography variant="button" color="primary.contrastText">
-                            Total Quakes Today
+                        <Typography variant="body1" color="primary.contrastText">
+                            Total Events
                         </Typography>
                         <Divider sx={{ backgroundColor: "primary.contrastText" }}/>
-                        <Typography variant="h4" color="primary.contrastText" mt={"0.5em"}>
+                        <Typography variant="h5" color="primary.contrastText" my={{ xs: "1.5em", sm: 0 }}>
                             {loading 
                                 ? <Skeleton sx={{ backgroundColor: "primary.light" }}/> 
                                 : totalQuakes}
@@ -51,37 +64,80 @@ const DataCards = () => {
                     </CardContent>
                 </DataCard>
             </Grid>
+
             <Grid item xs={6} sm={3} md={12}>
                 <DataCard>
                     <CardContent>
-                        <Typography variant="button" color="primary.contrastText">
+                        <Typography variant="body1" component="h3" color="primary.contrastText">
                             Largest Magnitude
                         </Typography>
                         <Divider sx={{ backgroundColor: "primary.contrastText" }}/>
-                        <Typography variant="h4" color="primary.contrastText" mt={"0.5em"}>
-                            {loading
-                                ? <Skeleton sx={{ backgroundColor: "primary.light" }}/> 
-                                : largestMag.properties.mag}
-                        </Typography>
-                        <Typography variant="button" color="primary.contrastText" mt={"0.5em"}>
-                            {loading
-                                ? <Skeleton sx={{ backgroundColor: "primary.light" }}/> 
-                                : largestMag.properties.place}
-                        </Typography>
-                        <Button variant="outlined" size="small" color='warning'>See On Map</Button>
+                        {loading 
+                            ? <Skeleton sx={{ height: "8em", backgroundColor: "primary.light" }}/>
+                            : <>
+                            <Typography variant="h5" color="primary.contrastText">
+                                { largestMag.properties.mag }
+                            </Typography>
+                            <Typography variant="caption" color="primary.contrastText" display={"flex"}>
+                                { largestMag.properties.place }
+                            </Typography>
+                            <Button variant="outlined" size="small" color='warning' startIcon={ <GpsFixedIcon /> } sx={{ margin: "0.5em" }} >
+                                Map
+                            </Button>
+                            </>
+                        }
                     </CardContent>
                 </DataCard>
             </Grid>
+
             <Grid item xs={6} sm={3} md={12}>
                 <DataCard>
                     <CardContent>
-                        <Typography variant="button" color="primary.contrastText">
+                        <Typography variant="body1" color="primary.contrastText">
                             Potential Damage
                         </Typography>
                         <Divider sx={{ backgroundColor: "primary.contrastText" }}/>
+                        {loading 
+                            ? <Skeleton sx={{ height: "5em", backgroundColor: "primary.light" }}/>
+                            : <>
+                            <Typography variant="h5" color="primary.contrastText">
+                                { significant.length }
+                            </Typography>
+                            {tsunami < 1 && 
+                                <Button variant="outlined" size="small" color='warning' startIcon={ <GpsFixedIcon /> } sx={{ margin: "0.5em" }} >
+                                    Map
+                                </Button>
+                            }
+                        </>
+                        }
                     </CardContent>
                 </DataCard>
             </Grid>
+
+            <Grid item xs={6} sm={3} md={12}>
+                <DataCard>
+                    <CardContent>
+                        <Typography variant="body1" color="primary.contrastText">
+                            Tsunami Potential
+                        </Typography>
+                        <Divider sx={{ backgroundColor: "primary.contrastText" }}/>
+                        {loading 
+                            ? <Skeleton sx={{ height: "5em", backgroundColor: "primary.light" }}/>
+                            : <>
+                            <Typography variant="h5" color="primary.contrastText">
+                                { tsunami.length }
+                            </Typography>
+                            {tsunami.length < 1 && 
+                                <Button variant="outlined" size="small" color='warning' startIcon={ <GpsFixedIcon /> } sx={{ margin: "0.5em" }}>
+                                    Map
+                                </Button>
+                            }
+                        </>
+                        }
+                    </CardContent>
+                </DataCard>
+            </Grid>
+
         </Grid>
     );
 };
--- src/utils/DataParser.js
+++ src/utils/DataParser.js
@@ -1,8 +1,11 @@
+const USGS_SIGNIFICANT_DEFINITION = 600;
+
 export const totalQuakes = (data) => {
   return data.features.length;
 };
 
 export const largestMagnitude = (data) => {
+    console.log(data);
     let largest = null;
     data.forEach((quake) => {
         if (!largest) {
@@ -13,3 +16,23 @@ export const largestMagnitude = (data) => {
     });
     return largest;
 };
+
+export const significantQuakes = (data) => {
+    let significant = [];
+    data.forEach((quake) => {
+        if (quake.properties.sig > USGS_SIGNIFICANT_DEFINITION) {
+            significant.push(quake);
+        }
+    });
+    return significant;
+};
+
+export const tsunamiPotential = (data) => {
+    let potential = [];
+    data.forEach((quake) => {
+        if (quake.properties.tsunami === 1) {
+            potential.push(quake);
+        }
+    });
+    return potential;
+}
