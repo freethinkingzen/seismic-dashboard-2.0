b0c768db6b090b438d948b4b5c9f54859e2379a4
John Lewis <johnlewis208503@gmail.com>
2024-01-07 14:20:00 -0700

    start on location/reset map control

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import MarkerFactory from "./MarkerFactory";
 import { SeismicDataContext } from "../Context";
+import PositionControl from "./PositionControl";
 
 
 const Listener = () => {
@@ -23,6 +24,7 @@ export default function Map() {
                 attribution= '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
             />
+            <PositionControl position="topright"/>
             <MarkerFactory />
             <Listener />
         </MapContainer>
---
+++ src/components/PositionControl.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useMap } from 'react-leaflet';
+import { IconButton, Tooltip } from '@mui/material';
+import { GpsFixed } from '@mui/icons-material';
+import styled from '@emotion/styled';
+
+const StyledButton = styled(IconButton)(({theme}) => ({
+    padding: "5px",
+    backgroundColor: "white",
+    color: "black",
+    '&:hover': {
+        backgroundColor: "#D0D0D8",
+        color: "black",
+    },
+}));
+
+
+function PositionControl() {
+    const map = useMap();
+
+    const isCentered = () => {
+        const defaultCenter = [20, 0];
+        const center = map.getCenter();
+        const centerCoordinates = [center.lat, center.lng];
+        const zoom = map.getZoom();
+        if(centerCoordinates === defaultCenter && zoom === 2) {
+            console.log("Centered");
+            return true;
+        } else {
+            console.log(centerCoordinates, defaultCenter, zoom);
+            return false;
+        }
+    }
+
+    const handleClick = () => {
+        if(!isCentered()) {
+            map.flyTo([20, 0], 2);
+        } else {
+            console.log("Centered");
+        }
+    };
+
+    return (
+        <div className={"leaflet-top leaflet-right"}>
+            <div className={"leaflet-control"}>
+                <Tooltip title={isCentered() ? "Your Location" : "Reset" } placement="bottom">
+                <StyledButton onClick={handleClick}>
+                    <GpsFixed />
+                </StyledButton>
+                </Tooltip>  
+            </div>
+        </div>
+    );
+}
+
+export default PositionControl;
